e63c4d351e88885c5db9926cf9d08d4ea1618d34
srdrakcay <srdrakcay25@gmail.com>
2023-10-15 19:45:01 +0300

    path and bitmap in circle

--- app/src/main/java/com/serdar/customviews_android/CustomCircleStrokeImage.kt
+++ app/src/main/java/com/serdar/customviews_android/CustomCircleStrokeImage.kt
@@ -25,13 +25,9 @@ class CustomCircleStrokeImage : View {
     private var circleXCenter=0f
     private var circleYCenter=0f
     private var circleRadius=0f
-    private var paint: Paint=Paint()
     private var mBitmap: Bitmap? = null
     private val path = Path()
     private val rectF = RectF()
-    private var ringColor = Color.BLACK
-    private var ringWidth = 5f
-    private var rainbow = false
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
@@ -49,23 +45,26 @@ class CustomCircleStrokeImage : View {
         circleRadius=h* CIRCLE_RADIUS_POS_FRACTION
 
     }
-    private val ringPaint = Paint().apply {
-        isAntiAlias = true
-        style = Paint.Style.STROKE
-        strokeWidth = ringWidth
-        color=ringColor
-        isFilterBitmap = true
-        isDither = true
-    }
     @SuppressLint("DrawAllocation")
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        val paint = Paint().apply {
+            isAntiAlias = true
+            style = Paint.Style.STROKE
+            strokeWidth = 10f
+            color=Color.BLACK
+            isFilterBitmap = true
+            isDither = true
+
+        }
         mBitmap = BitmapFactory.decodeResource(resources, R.drawable.un_filtered)
         val bitmap = mBitmap
         bitmap?.let {
-            val shader = BitmapShader(it, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
-            paint.shader = shader
-            val radius = (width.coerceAtMost(height) / 2 - ringPaint.strokeWidth / 2).toFloat()
+
+            //Circle Radius
+            val radius = (width.coerceAtMost(height) / 2 - paint.strokeWidth / 2).toFloat()
+
+            //Setting the dimensions
             rectF.set(0f, 0f, width.toFloat(), height.toFloat())
             path.addCircle(
                 rectF.centerX(),
@@ -73,9 +72,13 @@ class CustomCircleStrokeImage : View {
                 (rectF.width() / 2).coerceAtMost(rectF.height() / 2),
                 Path.Direction.CCW
             )
+            //Path for bitmap
             canvas?.clipPath(path)
+            //Drawing bitmap
             canvas?.drawBitmap(bitmap, null, rectF, paint)
-            canvas?.drawCircle(width / 2f, height / 2f, radius + ringPaint.strokeWidth / 2, ringPaint)
+
+            //Draw Circle
+            canvas?.drawCircle(width / 2f, height / 2f, radius + paint.strokeWidth / 2, paint)
         }
 
     }
